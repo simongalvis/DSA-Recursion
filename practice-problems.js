b75ba1b0ff9e2d91e9edce7127b9ba72dbf06798
Simon Galvis <simongalvis9@gmail.com>
2020-11-18 17:25:17 -0700

    Add example problem

--- practice-problems.js
+++ practice-problems.js
@@ -1,1 +1,13 @@
+//Example problem
+const sumOf = function(list) {
+    // Base case
+    if (list.length === 1) {
+        return list[0];
+    }
+    // General case
+    return list[0] + sumOf(list.slice(1));
 
+}
+
+let lst = [2,4,6,8,10];
+console.log(sumOf(lst));
