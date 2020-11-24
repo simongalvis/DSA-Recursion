2341348aa2c473e4fcced9c32e85f09f302692a5
Simon Galvis <simongalvis9@gmail.com>
2020-11-23 22:13:16 -0700

    Complete problem 4: Factorial

--- practice-problems.js
+++ practice-problems.js
@@ -10,7 +10,7 @@ const sumOf = function(list) {
 }
 
 let lst = [2,4,6,8,10];
-console.log(sumOf(lst));
+//console.log(sumOf(lst));
 
 // 1. Counting sheep
 
@@ -24,7 +24,7 @@ const sheepOverFence = function(sheepCount){
 }
 
 const sheepCount = 7;
-console.log(sheepOverFence(sheepCount));
+//console.log(sheepOverFence(sheepCount));
 
 // 2. Power Calculator
 
@@ -44,7 +44,7 @@ const powerCalculator = function(base, exponent){
   return base * powerCalculator(base, exponent-1);
 }
 
-console.log(powerCalculator(10, 2));
+//console.log(powerCalculator(10, 2));
 
 // 3. Reverse String
 
@@ -57,7 +57,7 @@ const reverseString = function(string){
    return string[string.length -1] + reverseString(string.slice(0, string.length -1));
 }
 
-console.log(reverseString('Reverse a string'));
+//console.log(reverseString('Reverse a string'));
 
 // 4. nth Triangular number
 
@@ -70,7 +70,7 @@ const nthTriangularNum = function(n){
   return  n + nthTriangularNum(n-1);
 }
 
-console.log(nthTriangularNum(5));
+//console.log(nthTriangularNum(5));
 
 // 5. String Splitter
 
@@ -83,4 +83,43 @@ const stringSplitter = function(string, separator){
   return  stringSplitter(string.replace(separator, ','), separator);
 }
 
-console.log(stringSplitter('11/18/2020', '/'))
+//console.log(stringSplitter('11/18/2020', '/'));
+
+// 6. Fibonacci
+
+const fibonacci = function(n){
+  //Base case
+  if(n === 0){
+    return 0;
+  }
+  else if(n === 1){
+    return 1;
+  }
+  //General case
+  return (fibonacci(n-1) + fibonacci(n-2))
+}
+
+//console.log(fibonacci(7))
+
+// 7. Factorial
+
+const factorial = (n) =>{
+  //Base case
+  if(n == 0){
+    return 1;
+  }
+  //General case
+  return n * factorial(n-1)
+}
+
+//console.log(factorial(7))
+
+// 8. Find a way out of the maze
+
+// 9. Find ALL the ways out of the maze
+
+// 10. Anagrams
+
+// 11. Organization chart
+
+// 12. Binary representation
